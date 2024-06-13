e97f22d0f4c922ee793df51692e43172790fc3c2
Brijesh Kumar <bk10895@gmail.com>
2024-06-13 16:12:07 +0530

    feat: add Mongoose for MongoDB connection and React-Icons for UI enhancement
    - Added Mongoose to connect with MongoDB for database operations
    - Included React-Icons for user-friendly UI
    - Constructed a basic Navbar component for navigation
    - Updated package.json and package-lock.json with new dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
+import { Navbar } from '@/components';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="mx-auto max-w-3xl p-4">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react';
+
+export const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between bg-slate-800 px-8 py-3">
+      <Link className="font-bold text-white" href="/">
+        Home
+      </Link>
+      <Link className="rounded bg-white p-2" href="/">
+        Add Topic
+      </Link>
+    </nav>
+  );
+};
