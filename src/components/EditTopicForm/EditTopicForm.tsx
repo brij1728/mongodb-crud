8247f53d666d5f9f0dd90008602e1f6b0c1b1ede
Brijesh Kumar <bk10895@gmail.com>
2024-06-13 17:59:38 +0530

    feat: enhance EditTopicForm component with form fields
    - Add form structure to the EditTopicForm component
    - Include input fields for 'Topic Title' and 'Topic Description'
    - Add 'Update Topic' button to submit the form

--- src/components/EditTopicForm/EditTopicForm.tsx
+++ src/components/EditTopicForm/EditTopicForm.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
 export const EditTopicForm = () => {
-  return <div>Edit Topic Form</div>;
+  return (
+    <form className="flex flex-col gap-3">
+      <input
+        className="rounded border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      />
+      <input
+        className="rounded border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
+
+      <button
+        className="text-bold w-full rounded bg-slate-700 px-5 py-3 text-white"
+        type="submit"
+      >
+        Update Topic
+      </button>
+    </form>
+  );
 };
