53cd830f80dccf9c01e8a775ed70de4a54d62d7d
Brijesh Kumar <bk10895@gmail.com>
2024-06-14 10:32:51 +0530

    feat: implement additional CRUD operations for topics
    - Add GET operation to fetch all topics
    - Implement PUT operation to update a topic by ID
    - Add DELETE operation to remove a topic by ID
    - Implement error handling for each operation

--- src/app/api/topics/route.ts
+++ src/app/api/topics/route.ts
@@ -33,3 +33,90 @@ export const POST = async (req: NextRequest) => {
     );
   }
 };
+
+export const GET = async () => {
+  await connectMongoDB();
+
+  try {
+    const topics = await Topic.find();
+    return NextResponse.json({ topics });
+  } catch (error: unknown) {
+    return NextResponse.json(
+      { error: 'Error fetching topics' },
+      { status: 500 }
+    );
+  }
+};
+
+export const PUT = async (req: NextRequest) => {
+  await connectMongoDB();
+
+  try {
+    const body = await req.json();
+    const {
+      id,
+      title,
+      description,
+    }: { id: string; title: string; description: string } = body;
+
+    if (!id || !title || !description) {
+      return NextResponse.json(
+        { error: 'ID, title, and description are required' },
+        { status: 400 }
+      );
+    }
+
+    const topic = await Topic.findByIdAndUpdate(
+      id,
+      { title, description },
+      { new: true }
+    );
+
+    if (!topic) {
+      return NextResponse.json({ error: 'Topic not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      {
+        message: 'Topic updated successfully',
+        topic,
+      },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    return NextResponse.json(
+      { error: 'Error updating topic' },
+      { status: 500 }
+    );
+  }
+};
+
+export const DELETE = async (req: NextRequest) => {
+  await connectMongoDB();
+
+  try {
+    const id = req.nextUrl.searchParams.get('id');
+
+    if (!id) {
+      return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+    }
+
+    const topic = await Topic.findByIdAndDelete(id);
+
+    if (!topic) {
+      return NextResponse.json({ error: 'Topic not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      {
+        message: 'Topic deleted successfully',
+      },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    return NextResponse.json(
+      { error: 'Error deleting topic' },
+      { status: 500 }
+    );
+  }
+};
