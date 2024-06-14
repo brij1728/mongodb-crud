15597d03875d351eb7ac4471fc67923fca54d692
Brijesh Kumar <bk10895@gmail.com>
2024-06-14 14:28:07 +0530

    feat: update API_URL to accommodate production environment
    - Modify API_URL in next.config file to switch between local and production URL based on NODE_ENV
    - Add MONGODB_URL to environment variables for database connection

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   env: {
     PORT: process.env.PORT,
-    API_URL: `http://localhost:${process.env.PORT}`,
+    API_URL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://mongodb-crud-ten.vercel.app/' // Replace with your production URL
+        : `http://localhost:${process.env.PORT}`,
+    MONGODB_URL: process.env.MONGODB_URL,
   },
 };
 
